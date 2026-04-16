6fda051eaeac42f1178cec7e4d3b5c5ed46da060
HokLoka <loka1713hk@gmail.com>
2026-04-16 12:47:25 +0800

    Update Cocktail_sort_demonstration.py

--- Task 2/Cocktail_sort_demonstration.py
+++ Task 2/Cocktail_sort_demonstration.py
@@ -3,17 +3,24 @@ def cocktail_sort(arr):
     start = 0
     end = n - 1
     swapped = True
-
+# Summary
+# Obtain the length of the input array
+# confirm the end point and starting point
+# Summary
+    
     while swapped:
         swapped = False
 
         # Forward pass: move the largest element to the end
         for i in range(start, end):
             if arr[i] > arr[i + 1]:
-                arr[i], arr[i + 1] = arr[i + 1], arr[i]
+                arr[i], arr[i + 1] = arr[i + 1], arr[i] #swapping two element if element i > element i+1
                 swapped = True
-        print("After forward pass:", arr)
-
+        print("After forward pass:", arr) # return the process
+# Summary
+# sort list from start to end
+# Summary
+        
         if not swapped:
             break
 
@@ -21,23 +28,26 @@ def cocktail_sort(arr):
         end -= 1
 
         # Backward pass: move the smallest element to the start
-        for i in range(end - 1, start - 1, -1):
+        for i in range(end - 1, start - 1, -1): # as the end of the array is sorted, so -1 here
             if arr[i] > arr[i + 1]:
-                arr[i], arr[i + 1] = arr[i + 1], arr[i]
+                arr[i], arr[i + 1] = arr[i + 1], arr[i] #swapping two element if element i > element i+1
                 swapped = True
-        print("After backward pass:", arr)
+        print("After backward pass:", arr) # return the process
 
         start += 1
 
     return arr
+# Summary
+# sort array from end to start and fially return the fully sorted array
+# Summary
 
-
-# Main program
+# Main body
 numbers = []
 print("Enter 5 numbers:")
 for _ in range(5):
     num = int(input("Number: "))
     numbers.append(num)
+# Enter 5 number and put to an array
 
 print("Original list:", numbers)
 sorted_list = cocktail_sort(numbers)
